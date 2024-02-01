6ad05a80fe7589b19c246c1e7700be11ad858cd0
jonnie254 <johnkamanu254@proton.me>
2024-02-01 13:57:16 +0300

    added some few features

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  * @type {HTMLElement}
  */
 const numberHours=document.querySelector('.number-hours');
+const barSeconds = document.querySelector('.bar-seconds');
 const numberElement=[];
+const barElement=[];
+
  
 //create number hours
 for(let i=1;i <=12;i++){
@@ -13,3 +16,11 @@ for(let i=1;i <=12;i++){
    
 }
 numberHours.insertAdjacentHTML("afterbegin", numberElement.join(""));
+
+//create bar seconds
+for(let i=1; i<=60; i++){
+    barElement.push(
+        `<span style="--index:${i};"><p></p></span>`
+    );
+}
+barSeconds.insertAdjacentHTML("afterbegin", barElement.join(""));
